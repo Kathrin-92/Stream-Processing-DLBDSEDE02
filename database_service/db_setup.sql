7c606a75b568b8eb5cca11405e7136ef204e9179
Kathrin Hälbich <kathrin.haelbich@iubh.de>
2025-03-12 12:37:29 +0100

    add metadata table to setup

--- database_service/db_setup.sql
+++ database_service/db_setup.sql
@@ -1,12 +1,12 @@
 -- will keep historical record of all sensor data datapoints
 -- will be an append-mode table, meaning all records will be kept and the table will get continuously larger
 CREATE TABLE IF NOT EXISTS airquality_raw (
-    id SERIAL PRIMARY KEY,
+    record_id SERIAL PRIMARY KEY,
     station_id INT,
     pollutant_id INT,
     pollutant_name TEXT,
     unit TEXT,
-    value TEXT,
+    value FLOAT,
     timestamp_start TIMESTAMP,
     timestamp_end TIMESTAMP,
     ingestion_timestamp TIMESTAMP
