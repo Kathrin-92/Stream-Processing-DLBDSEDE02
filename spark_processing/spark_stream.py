b73939abbccc3bb450378e4cce017da07457154e
Kathrin Hälbich <kathrin.haelbich@iubh.de>
2025-03-19 12:06:29 +0100

    adds aggregated data to postgres table

--- spark_processing/spark_stream.py
+++ spark_processing/spark_stream.py
@@ -35,11 +35,11 @@ spark = SparkSession.builder \
         .config("spark.sql.shuffle.partitions", 4) \
         .config("spark.jars", "/opt/spark/jars/postgresql-42.6.0.jar") \
         .getOrCreate()
-logger.info(f"Step (1): Spark Streaming Session launched.")
+logger.info(f"Step (1️⃣): Spark Streaming Session launched.")
 
 # test the connection and let spark read the db schema from the airquality_raw table
 # https://spark.apache.org/docs/latest/sql-data-sources-jdbc.html
-logger.info(f"Step (2): Testing connection to PostgreSQL database...")
+logger.info(f"Step (2️⃣): Testing connection to PostgreSQL database...")
 df = spark.read \
     .format("jdbc") \
     .option("url", "jdbc:postgresql://postgres_db:5432/airquality_sensor_data") \
@@ -50,8 +50,8 @@ df = spark.read \
     .load()
 
 schema_str = str(df.schema)
-logger.info(f"print schema: {schema_str}")
-logger.info(f"Connection to PostgreSQL successfull! Spark read table schema from airquality_raw.")
+logger.info(f"... ✅ Connection to PostgreSQL successfull! Spark read table schema from airquality_raw.")
+logger.info(f"... Print table schema: {schema_str}")
 
 # ----------------------------------------------------------------------------------------------------------------------
 # READ METADATA
@@ -66,13 +66,15 @@ airquality_metadata = spark.read \
     .option("driver", "org.postgresql.Driver") \
     .load()
 
-logger.info(f"Connection to PostgreSQL successfull! Spark read meatadata info on airquality data.")
+metadata_schema_str = str(airquality_metadata.schema)
+logger.info(f"... ✅ Connection to PostgreSQL successfull! Spark read meatadata info on airquality data.")
+logger.info(f"... Print metadata table schema: {metadata_schema_str}")
 
 # ----------------------------------------------------------------------------------------------------------------------
 # HISTORICAL RECORD
 # ----------------------------------------------------------------------------------------------------------------------
 
-logger.info(f"Step (3): Creating Spark Stream for incoming raw streaming data. Start processing...")
+logger.info(f"Step (3️⃣): Creating Spark Stream for incoming raw streaming data. Start processing...")
 
 # define json data schema for the incoming streaming data
 json_schema = StructType([
@@ -83,12 +85,12 @@ json_schema = StructType([
     StructField("value", FloatType(), True),
     StructField("timestamp_str", StringType(), True)
 ])
-logger.info(f"... successfully defined json schema for streaming data.")
+logger.info(f"... ✅ (RAW) successfully defined raw json schema for streaming data.")
 
 stream_df_base = spark.readStream.schema(json_schema).json("/api_service/sensor_data/stream_data")
-logger.info(f"... successfully loaded raw json file.")
+logger.info(f"... ✅ (RAW) successfully loaded raw json file.")
 
-logger.info(f"... start preprocessing data.")
+logger.info(f"... (RAW) start preprocessing raw data.")
 cast_date_from_timestamp = stream_df_base.withColumn('datetime_from', from_unixtime(col("datetime_from")).cast(TimestampType()))
 cast_date_to_timestamp = cast_date_from_timestamp.withColumn('datetime_to', from_unixtime(col("datetime_to")).cast(TimestampType()))
 with_ingestion_ts = cast_date_to_timestamp.withColumn("ingestion_timestamp", current_timestamp())
@@ -101,7 +103,7 @@ with_renamed_columns = with_metadata.withColumnsRenamed({
     "datetime_to": "timestamp_end"
 })
 with_id = with_renamed_columns.withColumn("record_id", hash("station_id", "pollutant_id", "value", "timestamp_start", "ingestion_timestamp"))
-logger.info(f"... successfully preprocessed data. Converted data types, added new columns, and joined metadata.")
+logger.info(f"... ✅ (RAW) successfully preprocessed raw data.")
 
 df_airquality_raw = with_id.select (
     col("record_id"),
@@ -115,51 +117,26 @@ df_airquality_raw = with_id.select (
     col("timestamp_end"),
     col("ingestion_timestamp")
 )
-logger.info(f"... selected necessary data columns to write to PostgreSQL.")
+logger.info(f"... ✅ (RAW) selected necessary data columns to write to PostgreSQL.")
 
-query_raw = df_airquality_raw.writeStream \
-    .foreachBatch(lambda batch_df, batch_id: batch_df.write \
-        .format("jdbc") \
-        .option("url", "jdbc:postgresql://postgres_db:5432/airquality_sensor_data") \
-        .option("dbtable", "airquality_raw") \
-        .option("user", "postgres") \
-        .option("password", "password") \
-        .option("driver", "org.postgresql.Driver") \
-        .mode("append") \
-        .save()) \
-    .start()
-logger.info(f"Step (4): Writing streaming data to airquality_raw PostgreSQL table... this is a test, why doesnt it work?!??!")
 
 # ----------------------------------------------------------------------------------------------------------------------
 # AGGREGATED DATA
 # ----------------------------------------------------------------------------------------------------------------------
 
-logging.info("Checking df_airquality_raw BEFORE aggregating:")
-logging.info(f"Schema: {df_airquality_raw.schema.simpleString()}")
-logging.info(f"Execution Plan:\n{df_airquality_raw.explain(extended=True)}")
-
-logger.info(f"Step (5): Start aggregating data...")
+logger.info(f"Step (4️⃣): Start aggregating data with adjusted window...")
 aggregated_data = (df_airquality_raw
-    .withWatermark("ingestion_timestamp", "10 minutes")
-    .groupBy("pollutant_id", "pollutant_name", "pollutant_symbol", "unit", window(col("ingestion_timestamp"), "5 minutes", "1 minute"))
+    .withWatermark("ingestion_timestamp", "5 minutes")
+    .groupBy("pollutant_id", "pollutant_name", "pollutant_symbol", "unit")
     .agg(
         max(col("value")).alias("max_value"),
         min(col("value")).alias("min_value"),
-        avg(col("value")).alias("avg_value"),
-))
+        avg(col("value")).alias("avg_value"))
+    .withColumn("last_updated", current_timestamp())
+)
+logger.info(f"... ✅ (AGG) aggregated data successfully.")
 
-logging.info("🔍 Checking aggregated_data BEFORE adding current timestamp:")
-logging.info(f"Schema: {aggregated_data.schema.simpleString()}")
-logging.info(f"Execution Plan:\n{aggregated_data.explain(extended=True)}")
-
-with_last_updated = aggregated_data.withColumn("last_updated", current_timestamp())
-logger.info(f"... aggregated data successfully.")
-
-logging.info("🔍 Checking with_last_updated BEFORE last select:")
-logging.info(f"Schema: {with_last_updated.schema.simpleString()}")
-logging.info(f"Execution Plan:\n{with_last_updated.explain(extended=True)}")
-
-df_airquality_aggregated = with_last_updated.select (
+df_airquality_aggregated = aggregated_data.select (
     col("pollutant_id"),
     col("pollutant_name"),
     col("pollutant_symbol"),
@@ -169,16 +146,36 @@ df_airquality_aggregated = with_last_updated.select (
     col("avg_value"),
     col("last_updated")
 )
-logger.info(f"... selected necessary data columns to write to PostgreSQL.")
+logger.info(f"... ✅ (AGG) selected necessary data columns to write to PostgreSQL.")
 
-logging.info("🔍 Checking df_airquality_aggregated BEFORE starting query_agg:")
-logging.info(f"Schema: {df_airquality_aggregated.schema.simpleString()}")
-logging.info(f"Execution Plan:\n{df_airquality_aggregated.explain(extended=True)}")
-logging.info("print this")
 
-# to do: this does not work, always 0 rows in the table
-query_agg = df_airquality_aggregated.writeStream \
+# ----------------------------------------------------------------------------------------------------------------------
+# START STREAMING QUERIES
+# ----------------------------------------------------------------------------------------------------------------------
+
+query_raw = df_airquality_raw.writeStream \
+    .outputMode("append") \
     .foreachBatch(lambda batch_df, batch_id: batch_df.write \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://postgres_db:5432/airquality_sensor_data") \
+        .option("dbtable", "airquality_raw") \
+        .option("user", "postgres") \
+        .option("password", "password") \
+        .option("driver", "org.postgresql.Driver") \
+        .mode("append") \
+        .save()) \
+    .option("checkpointLocation", "/usr/src/spark_processing/checkpoint/raw") \
+    .start()
+logger.info(f"Step (5️⃣): (RAW) Start query_raw. Writing streaming data to airquality_raw PostgreSQL table...")
+
+query_agg = df_airquality_aggregated.writeStream \
+    .outputMode("update") \
+    .foreachBatch(lambda batch_df, batch_id: (
+        logger.info(f"... (AGG) Processing batch {batch_id} with {batch_df.count()} rows."),
+
+        # maybe add custom merge logic here to only update aggregates and last_updated column
+
+        batch_df.write \
         .format("jdbc") \
         .option("url", "jdbc:postgresql://postgres_db:5432/airquality_sensor_data") \
         .option("dbtable", "airquality_aggregated") \
@@ -186,26 +183,16 @@ query_agg = df_airquality_aggregated.writeStream \
         .option("password", "password") \
         .option("driver", "org.postgresql.Driver") \
         .mode("append") \
-        .save()) \
+        .save())) \
+    .option("checkpointLocation", "/usr/src/spark_processing/checkpoint/agg") \
     .start()
-logger.info(f"Step (6): Writing aggregated streaming data to airquality_aggregated PostgreSQL table...")
-
-def debug_batch(batch_df, batch_id):
-    logging.info(f"Processing batch {batch_id} with {batch_df.count()} rows")
-    if batch_df.isEmpty():
-        logger.info(f"Batch {batch_id} is empty.")
-    else:
-        logger.info(f"Batch {batch_id} has {batch_df.count()} records.")
-        logging.info(f"Sample data from batch {batch_id}:\n{batch_df.show(5, truncate=False)}")
-
-query_debug = df_airquality_aggregated.writeStream \
-    .foreachBatch(debug_batch) \
-    .start()
-
-logger.info("try debugging")
-
-for q in spark.streams.active:
-    logging.info(f"Query {q.name}: {q.status}")
-    logging.info(f"Last Progress:\n{q.lastProgress}")
+logger.info(f"Step (6️⃣): (AGG) Start query_agg. Writing aggregated streaming data to airquality_aggregated PostgreSQL table...")
 
 spark.streams.awaitAnyTermination()
+
+# to do: build logic to delete files that have been processed by agg after the five minutes retention time window
+
+# for debugging
+# for q in spark.streams.active:
+#     logger.info(f"Query {q.name}: {q.status}")
+#     logger.info(f"Last Progress:\n{q.lastProgress}")
